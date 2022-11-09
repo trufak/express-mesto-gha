19e9d142c3617f077bbd78add745ffc53b2aeb45
Sergey Trufakin <trufakin.sergey@mail.ru>
2022-11-09 10:35:57 +0700

    fix: изменить проверку url в celebrate

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required().uri(),
+    avatar: Joi.string().required().uri({ scheme: ['http', 'https'] }),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
