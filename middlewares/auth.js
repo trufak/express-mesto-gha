a7e497c12d3626d76ecd3fa89bfd3610a77ef1a3
Sergey Trufakin <trufakin.sergey@mail.ru>
2022-11-12 21:07:02 +0700

    fix: исправить ошибки при аутентификации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -15,7 +15,8 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token,
+      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new UnauthorizedError(err.message));
   }
